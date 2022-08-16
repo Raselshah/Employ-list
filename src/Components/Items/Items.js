b303d18fc695794c237aa049b62078204749216b
Md Rasel Shah <mdraselshah56@gmail.com>
2022-08-16 22:32:32 +0600

    manager page added

---
+++ src/Components/Items/Items.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { TreeItem } from "@mui/lab";
+
+const Items = ({ id, first_name, herEmployes }) => {
+  console.log(id, first_name, herEmployes);
+  const hasChildren = first_name && herEmployes?.length;
+
+  return (
+    <>
+      <TreeItem nodeId={id} label={first_name}>
+        {hasChildren &&
+          herEmployes?.map((manager) => (
+            <Items key={manager.id} {...manager} />
+          ))}
+      </TreeItem>
+    </>
+  );
+};
+export default Items;
--- src/Components/Manager/Manager.js
+++ src/Components/Manager/Manager.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
-import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+
 import MuiAccordion from "@mui/material/Accordion";
-import MuiAccordionSummary from "@mui/material/AccordionSummary";
-import MuiAccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import { Box } from "@mui/material";
+
+import { TreeView } from "@mui/lab";
+import { ChevronRight, ExpandMore } from "@mui/icons-material";
 import axios from "axios";
+import Items from "../Items/Items";
 
 const Manager = () => {
   const [userInfo, setUserInfo] = useState([]);
@@ -28,17 +28,34 @@ const Manager = () => {
     user.manager_id.length > 0 ? managers.push(user) : ""
   );
 
-  const formatedData = [];
-
-  managers.forEach((employe) => {
-    let id = employe.id;
-    console.log(id);
-    const herEmployes = userInfo.filter((employe) => employe.manager_id === id);
-    if (herEmployes.length) {
-      const empWmanager = { ...employe, herEmployes };
-      formatedData.push(empWmanager);
+  const useEmployes = (employes) => {
+    const data = formateTheData(employes);
+    if (data.length === 0) {
+      return employes;
     }
-  });
+    return useEmployes(data);
+  };
+
+  const formateTheData = (employes) => {
+    const formatedData = [];
+
+    userInfo.forEach((employe) => {
+      const id = employe.id;
+      const herEmployes = employes.filter(
+        (employe) => employe.manager_id === id
+      );
+      if (herEmployes.length) {
+        const empWmanager = { ...employe, herEmployes };
+        formatedData.push(empWmanager);
+      }
+    });
+    return formatedData;
+  };
+
+  const managersData = useEmployes(userInfo);
+  if (managersData.length < 1) {
+    return;
+  }
 
   const Accordion = styled((props) => (
     <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -52,58 +69,17 @@ const Manager = () => {
     },
   }));
 
-  const AccordionSummary = styled((props) => (
-    <MuiAccordionSummary
-      expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
-      {...props}
-    />
-  ))(({ theme }) => ({
-    backgroundColor:
-      theme.palette.mode === "dark"
-        ? "rgba(255, 255, 255, .05)"
-        : "rgba(0, 0, 0, .03)",
-    flexDirection: "row-reverse",
-    "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
-      transform: "rotate(90deg)",
-    },
-    "& .MuiAccordionSummary-content": {
-      marginLeft: theme.spacing(1),
-    },
-  }));
-
-  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-    padding: theme.spacing(2),
-    borderTop: "1px solid rgba(0, 0, 0, .125)",
-  }));
-
-  const [expanded, setExpanded] = React.useState("panel1");
-
-  const handleChange = (panel) => (event, newExpanded) => {
-    setExpanded(newExpanded ? panel : false);
-  };
   return (
-    <Box sx={{ maxWidth: "1540px", padding: "15px" }}>
-      {formatedData.map((user) => (
-        <>
-          <Accordion
-            expanded={expanded === user.id}
-            onChange={handleChange(user.id)}
-          >
-            <AccordionSummary
-              aria-controls="panel1d-content"
-              id="panel1d-header"
-            >
-              <Typography>{user.first_name}</Typography>
-            </AccordionSummary>
-            {user.herEmployes.map((e) => (
-              <AccordionDetails>
-                <Typography>{e.first_name}</Typography>
-              </AccordionDetails>
-            ))}
-          </Accordion>
-        </>
+    <TreeView
+      aria-label="file system navigator"
+      defaultCollapseIcon={<ExpandMore />}
+      defaultExpandIcon={<ChevronRight />}
+      sx={{ width: 500, flexGrow: 1, mx: "auto", mt: 3 }}
+    >
+      {managersData.map((manager) => (
+        <Items key={manager.id} {...manager} />
       ))}
-    </Box>
+    </TreeView>
   );
 };
 
